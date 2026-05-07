83d8c7b4fe2f49bc26f81d19eb198ea1e11fdb89
zhangjunwang255-qwq <zhangjunwang255@gmail.com>
2026-05-07 12:39:08 +0800

    fix: 采用方案二，build阶段装依赖，deploy阶段启动

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd backend
-PYTHON_PATH=$(which python3)
-$PYPYTHON_PATH -m pip install -r requirements.txt --quiet
-exec $PYTHON_PATH -m uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m uvicorn main:app --host 0.0.0.0 --port $PORT
