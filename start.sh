6e842761c743a51fa0458758a1335c1598505d7f
zhangjunwang255-qwq <zhangjunwang255@gmail.com>
2026-05-07 12:33:26 +0800

    fix: 使用python3路径确保一致性

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd backend
-python -m uvicorn main:app --host 0.0.0.0 --port $PORT
+PYTHON_PATH=$(which python3)
+$PYPYTHON_PATH -m pip install -r requirements.txt --quiet
+exec $PYTHON_PATH -m uvicorn main:app --host 0.0.0.0 --port $PORT
